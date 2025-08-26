593265d6bdd8d1aed5b6be043035ce9fce5bee70
soumalya sen <Soumalya1990@gmail.com>
2025-08-26 15:20:09 +0000

    data type basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,58 @@
-console.log('hi')
+
+// Data type =====>
+
+//  1.premitive data type  
+// ------------------------------//
+
+// a. string 
+
+// let username = "Soumalya";
+// let message = `Hello, ${username}!`; 
+// console.log(message)
+// console.log(typeof message)
+
+// b. number
+
+// let age = 28;
+// let price = 199.99;
+//  console.log(price)
+//  console.log(typeof price)
+
+// c. boolean
+
+// let isLoggedIn = true;
+// if (isLoggedIn) {
+//   console.log("Welcome back!");
+// }
+// else{
+//    console.log("user not found") 
+// }
+
+// d. undefined
+
+// let user;
+// console.log(user); 
+
+// e. null
+
+// let selectedProduct = null;
+
+// f. symbol
+
+
+// g. bigint
+
+// let bigNumber = 123456789012345678901234567890n;
+
+  
+// 2.non premitive / reference type
+// ------------------------------//
+
+// a. Array
+
+// let numbers = [1, 2, 3, 4];
+
+// b. Object
+
+// let user = { name: "Krishiv", age: 1 };
+
