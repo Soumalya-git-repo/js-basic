41fb78e9965a6615157ee9ffbc4667ab893b81cf
soumalya sen <Soumalya1990@gmail.com>
2025-08-28 15:03:07 +0000

    objects

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -162,3 +162,183 @@
     //    console.log(str1.concat(str2)); // "Hello, world!"    
 
 // ===============Most Important & Commonly Used Number Methods =============//
+
+
+    // 1. toFixed(digits)  Formats a number using fixed-point notation.
+
+        // let price = 9.876;
+        // console.log(price.toFixed(2)); // "9.88"
+
+    // 2. toPrecision(digits)  Formats a number to a specified length.
+
+        // let num = 123.456;
+        // console.log(num.toPrecision(5)); // "123.46"
+
+    // 3. toLocaleString()  Converts number to string using locale settings.
+
+        // let num = 123456.789;
+        // console.log(num.toLocaleString("en-US")); // "123,456.789"
+        // console.log(num.toLocaleString("de-DE")); // "123.456,789"
+
+
+ //============ Number Properties =============
+
+        // Number.MAX_VALUE → Largest possible number.
+
+        // Number.MIN_VALUE → Smallest positive number.
+
+        // Number.POSITIVE_INFINITY / Number.NEGATIVE_INFINITY
+
+        // Number.NaN → Not-a-Number.
+
+        // console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
+        // console.log(Number.MIN_VALUE);  // 5e-324
+        // console.log(Number.NaN);        // NaN
+
+
+//   ====================== MATH =======================      
+// 1. Rounding Methods
+    // console.log(Math.round(4.6)); // 5
+    // console.log(Math.floor(4.9)); // 4
+    // console.log(Math.ceil(4.1));  // 5
+    // console.log(Math.trunc(4.9)); // 4
+
+// 2. Random Number (random range is between 0 and 1)
+    // console.log(Math.random());           // 0.12345...
+    // console.log((Math.random() * 10) + 1); // 1–10
+    // console.log(Math.floor(Math.random() * 10) + 1); 
+    // console.log(Math.round(Math.random() * 10) + 1); 
+
+    // const min = 10
+    // const max = 20
+
+    // console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// 3. Min & Max
+    // console.log(Math.min(5, 10)); // 5
+    // console.log(Math.max(5, 10)); // 10
+
+    // ==================Date ======================
+
+    // let date = new Date();
+
+// console.log(date.getFullYear());  // 2025
+// console.log(date.getMonth());     //(month)
+// console.log(date.getDate());      //(day of month)
+// console.log(date.getDay());       // (0 = Sunday, 1 = Monday...)
+// console.log(date.getHours());     // 20
+// console.log(date.getMinutes());   // 30
+// console.log(date.getSeconds());   //(seconds)
+// console.log(date.getMilliseconds()); //(milliseconds)
+// console.log(date.getTime());      //(timestamp in ms since 1970)
+//  console.log(date.toDateString()); 
+
+
+// =====================Array ==============================
+
+// An array is a special type of object used to store multiple values in a single variable.
+// Values inside an array are called elements.
+// Each element has an index (position), starting from 0
+
+
+// const myArr = [1, 2, 3, 4, 5]
+
+// console.log(myArr.push(6));
+// console.log(myArr.pop(3));
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(2));
+// console.log(myArr);
+
+
+// slice or splice 
+// note:
+// slice is non-destructive (does not change the original array).
+// splice is destructive (modifies the original array).
+
+// let nums = [10, 20, 30, 40, 50];
+
+// console.log(nums.slice(1, 4)); // [20, 30, 40]  (copy portion, no change)
+// console.log(nums);             // [10, 20, 30, 40, 50] (original array unchanged)
+
+// nums.splice(2, 1, 99);         // remove 1 element at index 2, insert 99
+// console.log(nums);             // [10, 20, 99, 40, 50]
+
+
+// methods of array
+
+// const marvelHeros = ["Iron Man", "Spider-Man", "Thor", "Hulk", "Black Widow"];
+
+// const dcHeros = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman"];
+
+// marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[5][0]);
+
+
+// const allHeros = marvelHeros.concat(dcHeros)
+//  console.log(allHeros);
+
+// const allNewHeros = [...marvelHeros, ...dcHeros]
+// console.log(allNewHeros);
+
+// const allnewArray = [1,2,3,[1,2,3],4,[5,6,7,[9,10]]];
+
+// console.log(allnewArray.flat(Infinity));
+
+
+// console.log(Array.from("soumalya"));
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3));
+
+// ******************  OBJECT  *******************
+// const id = Symbol("id");    // important for interview     
+
+// const person = {
+//     name: "John",
+//     lastName: "Doe",
+//     "full Name": "Jhon DOW",
+//     age: 30,
+//     [id]: 123, // symbol as key
+//     isStudent: false,
+//     greet: function() {
+//         console.log("Hello, my name is " + this.name);
+//     }
+// };
+
+// console.log(person.name);
+// console.log(person["age"]);
+// console.log(person["full Name"]);
+// console.log(typeof person[id]);
+// person.greet();
+
+// spread operator
+
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { b: 3, c: 4 };
+
+// const mergedObj = { ...obj1, ...obj2 };
+// console.log(mergedObj); // { a: 1, b: 3, c: 4 } (obj2's 'b' overwrites obj1's 'b')
+
+
+//-------- many objects inside one Array ------------
+
+// const users = [
+//     { name: "Alice", age: 25, isStudent: true },
+//     { name: "Bob", age: 30, isStudent: false },
+//     { name: "Charlie", age: 22, isStudent: true },
+//     { name: "Diana", age: 28, isStudent: false }
+// ];
+
+// console.log(users[1].age);
+
+// console.log(Object.keys(users[0])); 
+// console.log(Object.values(users[0])); 
+// console.log(users[0].hasOwnProperty("age")); // true
+// console.log(users[0].hasOwnProperty("gender")); // false
